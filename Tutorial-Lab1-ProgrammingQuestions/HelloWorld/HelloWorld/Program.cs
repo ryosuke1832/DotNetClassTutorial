c03f895bef5cb7e35efcf7e82cefecfaa4159fe3
Ryo <ryo@Ryos-MacBook-Air.local>
2025-07-29 14:50:26 +1000

    first2025-0729

---
+++ Tutorial-Lab1-ProgrammingQuestions/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Write a program to print Hello World 
+in the console
+ */
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write the code to print Hello World
+            Console.WriteLine("Hello World");
+
+
+            // Write the code to accept a single key press from the user
+            Console.ReadKey(); // Wait for a key press to keep the console open
+
+        }
+    }
+}
+
+/*
+Test case:
+
+Expected Output:
+Hello World
+
+*/
---
+++ tutorial/tutorial1/HelloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+ 
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey(); // Fixed typo in Console.ReadKey
+        }
+    }
+}
