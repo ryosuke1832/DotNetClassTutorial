6998f7512b611ea895a21dea6d5a37cce25d002a
Ryo <ryo@Ryos-MacBook-Air.local>
2025-09-02 12:31:06 +1000

    week6 end

--- Tutorial lab 6 ProgrammingQuestions/Week6Program1/Week8Program1/Program.cs
+++ Tutorial lab 6 ProgrammingQuestions/Week6Program1/Week8Program1/Program.cs
@@ -10,54 +10,74 @@ Use the class diagram given in the tutorial instruction document as reference.
 namespace Week6Program1
 {
     // Create the MyGenericStack class
-    public class 
+    public class MyGenericStack<T>
     {
-        // Write the code for the Properties
+        // Write the code for the Praoperties
         // 1. To Keep track of the number of elements added to the Stack in LIFO (Last In First Out) order
+        private int top;
 
         // 2. an array to store the element in the stack
+        private T[] stackArray;
 
         // Write code for the constructor which initializes the stack based on the value of myGenericStackSize, 
         // which should be greater than zero
-        public 
+        public MyGenericStack(int myGenericStackSize)
         {
             // Write code to Check if the stack size is >0
-            if ()
+            if (myGenericStackSize > 0)
             {
-               
+                stackArray = new T[myGenericStackSize];
+                top = -1;
             }
             else
-                // Write code to Throw an exception 
-                
+            {
+                throw new ArgumentException("Stack size must be greater than zero.");
+            }
         }
 
-        //Write code for the Puch() method to add an element in the stack
-        public  
+        //Write code for the Push() method to add an element in the stack
+        public void Push(T value)
         {
             // Check if the value of top<Size of stack: Stack empty
-            
+            if (top >= stackArray.Length - 1)
+            {
+                throw new StackOverflowException("Stack overflow: Cannot push to a full stack.");
+            }
+
             // Increment top
-            
+            top++;
             // Add the value into the stack
-            
+            stackArray[top] = value;
+
         }
 
         //Write code for the Pop() method which returns the current value at the location indicated by top
-        public 
+        public T Pop()
         {
             // Check if the value of top<0: Stack empty
-            
+            if (top < 0)
+            {
+                throw new ArgumentOutOfRangeException("Stack is empty.");
+            }
+
             // Decrement top
+            top--;
             
             // return the value of top +1: the element being deleted
-            
+            return stackArray[top + 1];
         }
 
         // Write code for Accessor: returns the size of stack
-        public int
+        public int Length
+        {
+            get { return stackArray.Length - 1; }
+        }
 
         // Write code for Accessor: returns the actual number of elements in the stack
-        public int 
+        public int NoElements
+        {
+            get { return top + 1; }
+        }
     }
 
 
@@ -66,7 +86,7 @@ namespace Week6Program1
     {
         // Initial static double and string arrays with potential values to be inserted into the stacks
         // The values can be user inputs as well
-        private static double[] doubleValues = new double[] { 1.2, 3.4, 5.6, 9.2, 4.5 };
+        private static double[] doubleValues = new double[] {1.2, 3.4, 5.6, 9.2, 4.5 };
 
         private static string[] stringValues = new string[] { "Paul", "George", "Alan", "Alex", "Martin"};
 
